799deb293fe7c119f1519a30c1652f807be0c151
ivan piskun <vassan@macbook-air-ivan.local>
2018-08-01 21:37:15 +0300

    Added x Variable to ViewController, addedFile

---
+++ GitSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitSample
+//
+//  Created by ivan piskun on 01.08.2018.
+//  Copyright © 2018 ivan piskun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var x = 10
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
